78586d3240f2da47323a7ce853c2cc60e8bf2d2f
Anekront <107873869+ASYarichuk@users.noreply.github.com>
2024-02-07 10:39:49 +0300

    Init

---
+++ Clamp.cs
@@ -0,0 +1,9 @@
+public static int Clamp(int value, int minValue, int maxValue)
+{
+    if (value < minValue)
+        return minValue;
+    else if (value > maxValue)
+        return maxValue;
+    else
+        return value;
+}
